2a91573db1e38159d33f4cbc0a807f5add8a395d
CielAl <cielmercy@gmail.com>
2025-09-02 07:38:25 -0400

    update

--- simclr_lightning/models/metrics/metric_dict.py
+++ simclr_lightning/models/metrics/metric_dict.py
@@ -1,5 +1,5 @@
 from torch import nn
-from simclr_lightning.models.lightning_modules.base import PHASE_TRAIN, PHASE_VAL, PHASE_TEST, PHASE_PRED, PHASE_STR
+from simclr_lightning.models.lightning_modules.base import PHASE_STR
 from typing import List, Type, Dict, get_args, cast, Callable
 import torchmetrics
 
@@ -10,7 +10,7 @@ class MetricDict(nn.ModuleDict):
         super().__init__()
 
     @classmethod
-    def build_from_args(cls, mode_list: List[PHASE_STR], metric: Type[torchmetrics.Metric], **kwargs):
+    def build_from_args(cls, mode_list: List[str], metric: Type[torchmetrics.Metric], **kwargs):
         m_dict = cls()
         for mode in mode_list:
             m_dict[mode] = metric(**kwargs)
@@ -22,18 +22,21 @@ class MetricDict(nn.ModuleDict):
         return cls.build_from_args(mode_list, metric, **kwargs)
 
     @classmethod
-    def build_dict(cls, metric_dict: Dict[PHASE_STR, torchmetrics.Metric]):
+    def build_dict(cls, metric_dict: Dict[str, torchmetrics.Metric]):
         m_dict = cls()
         for mode, metric in metric_dict.items():
             m_dict[mode] = metric
         return m_dict
 
-    def reset_all(self):
+    def reset_all(self, *args, **kwargs):
         for metric in self.values():
-            metric.reset()
+            metric.reset(*args, **kwargs)
 
-    def reset_by_mode(self, mode: PHASE_STR):
-        self[mode].reset()
+    def reset_by_mode(self, mode: str, *args, **kwargs):
+        if mode not in self.keys():
+            raise ValueError(f'mode {mode} is not supported')
+            # return
+        self[mode].reset(*args, **kwargs)
 
-    def get_metric(self, mode: PHASE_STR) -> torchmetrics.Metric:
+    def get_metric(self, mode: str) -> torchmetrics.Metric:
         return cast(torchmetrics.Metric, self[mode])
